36f294d90813bea0b9262b3902b8d38218d20b9b
romanas <romanas.chomskis@gmail.com>
2022-01-25 19:12:26 +0200

    componets implemented

--- react-exam/src/components/Skill.jsx
+++ react-exam/src/components/Skill.jsx
@@ -1,3 +1,17 @@
-import {useNavigate} from "react-router-dom";
+// import {useNavigate} from "react-router-dom";
+import { Card, CardItem, CardHeader, CardContent, CardNote} from "../ui/atoms/CardElements";
 
-export const Skill = ({pet, onDelete}) => {};
+export const Skill = ({skill}) => {
+    
+    return (
+        <Card>
+            <CardItem>
+                <CardHeader>{skill.title}</CardHeader>
+            </CardItem>
+            <CardContent>
+                <CardNote>{skill.description}</CardNote>
+            </CardContent>
+        </Card>
+    );
+    
+};
